28679374efdfc6271b49ced4a687c3a09f09e5fd
rant-net <34341872+rand-net@users.noreply.github.com>
2026-05-11 21:23:36 +0530

    feat: add base template and pages

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,178 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let theme = $state('light');
+
+	function applyTheme(newTheme) {
+		theme = newTheme;
+
+		document.documentElement.setAttribute('data-theme', theme);
+
+		localStorage.setItem('theme', theme);
+	}
+
+	onMount(() => {
+		// Close dropdowns
+		const closeDetails = (e) => {
+			document.querySelectorAll('details[open]').forEach((details) => {
+				if (!details.contains(e.target)) {
+					details.removeAttribute('open');
+				}
+			});
+		};
+
+		document.addEventListener('click', closeDetails);
+
+		const savedTheme = localStorage.getItem('theme');
+		const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+		if (savedTheme) {
+			theme = savedTheme;
+		} else {
+			theme = prefersDark ? 'dark' : 'light';
+		}
+
+		document.documentElement.setAttribute('data-theme', theme);
+
+		return () => {
+			document.removeEventListener('click', closeDetails);
+		};
+	});
+</script>
+
+<div class="navbar-container sticky top-0 z-30">
+	<!--Right Side -->
+	<div class="navbar bg-base-100 shadow-sm backdrop-blur-md">
+		<div class="navbar-start">
+			<div class="dropdown">
+				<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h8m-8 6h16"
+						/>
+					</svg>
+				</div>
+				<ul
+					tabindex="0"
+					class="dropdown-content menu z-[1] mt-3 w-52 menu-sm rounded-box bg-white p-2 shadow"
+				>
+					<li><a class="brand-link" href="/blog">Blog</a></li>
+					<li><a class="brand-link" href="/about">About</a></li>
+					<!--Theme Toggler-->
+					<li class="m-auto">
+						<label class="flex cursor-pointer gap-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="20"
+								height="20"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							>
+								<circle cx="12" cy="12" r="5" />
+								<path
+									d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
+								/>
+							</svg>
+							<input type="checkbox" value="light" class="theme-controller toggle" />
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="20"
+								height="20"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							>
+								<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+							</svg>
+						</label>
+					</li>
+				</ul>
+			</div>
+			<a href="/" class="brand-logo btn-ghost">
+				<img src="/images/byte-charts.svg" alt="Byte Charts Logo" class="logo-img" />
+			</a>
+		</div>
+
+		<!--Left Side-->
+		<div class="navbar-end hidden lg:flex">
+			<ul class="menu menu-horizontal relative z-10 px-1">
+				<li><a class="brand-link hvr-lines-square" href="/blog"> Blog </a></li>
+				<li><a class="brand-link hvr-lines-square" href="/about">About</a></li>
+
+				<!-- Theme Toggler -->
+				<li class="m-auto">
+					<label class="flex cursor-pointer gap-2">
+						<!-- sun -->
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<circle cx="12" cy="12" r="5" />
+							<path
+								d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
+							/>
+						</svg>
+
+						<input
+							type="checkbox"
+							class="toggle"
+							checked={theme === 'dark'}
+							onchange={(e) => applyTheme(e.currentTarget.checked ? 'dark' : 'light')}
+						/>
+
+						<!-- moon -->
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+						</svg>
+					</label>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<style>
+	.brand-link {
+		font-family: 'Space Grotesk', sans;
+		font-size: 1.575rem;
+		font-weight: 700;
+		letter-spacing: 0.0825em;
+	}
+	.brand-logo {
+		height: 64px;
+		width: 64px;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,30 @@
+<main>
+	<!--<div class="hero min-h-screen bg-base-200">-->
+	<!--<div class="hero-content text-left">-->
+	<!--<div class="max-w-md">-->
+	<!--<h1 class="text-5xl font-bold">About</h1>-->
+	<!--<p class="py-6">-->
+	<!--Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi-->
+	<!--exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.-->
+	<!--</p>-->
+	<!--</div>-->
+	<!--</div>-->
+	<!--</div>-->
+
+	<div class="hero min-h-screen bg-base-200">
+		<div class="hero-content flex-col lg:flex-row-reverse">
+			<img
+				src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+				class="max-w-sm rounded-lg shadow-2xl"
+			/>
+			<div>
+				<h1 class="text-5xl font-bold">Box Office News!</h1>
+				<p class="py-6">
+					Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+					exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+				</p>
+				<button class="btn btn-primary">Get Started</button>
+			</div>
+		</div>
+	</div>
+</main>
