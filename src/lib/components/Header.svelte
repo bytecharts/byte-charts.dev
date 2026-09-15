32f7ba0648c76338919b63d988176b84ac0a3937
rant-net <34341872+rand-net@users.noreply.github.com>
2026-09-15 12:21:40 +0530

    Fix(navbar): fixes theme toggle for mobile

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -97,7 +97,12 @@
 									d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
 								/>
 							</svg>
-							<input type="checkbox" value="bc-light" class="theme-controller toggle" />
+							<input
+								type="checkbox"
+								class="toggle"
+								checked={theme === 'bc-dark'}
+								onchange={(e) => applyTheme(e.currentTarget.checked ? 'bc-dark' : 'bc-light')}
+							/>
 
 							<!-- moon -->
 							<svg
